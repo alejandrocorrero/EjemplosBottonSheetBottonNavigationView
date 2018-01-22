8914a8cf86338b97dba3cf9438b0e1c07e2b29b0
Ordenador insti <32162937+alejandrocorrero@users.noreply.github.com>
2018-01-22 12:44:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/usuario/ejemplosbottonsheetbottonnavigationview/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.usuario.ejemplosbottonsheetbottonnavigationview;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.mnuFrg1:
+
+                                break;
+                            case R.id.mnuFrg2:
+
+                                break;
+                            case R.id.mnuFrg3:
+
+                                break;
+                        }
+                        return true;
+                    }
+                });
+    }
+}
